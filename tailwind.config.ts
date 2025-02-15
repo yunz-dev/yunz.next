617eb1c4e499563200bda4b45ba9f987c1d99796
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-02-15 21:01:03 +1100

    update: tailwindconfig

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,17 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        gray: {
+          100: "#f7fafc",
+          200: "#edf2f7",
+          300: "#e2e8f0",
+          400: "#cbd5e0",
+          500: "#a0aec0",
+          600: "#718096",
+          700: "#4a5568",
+          800: "#2d3748",
+          900: "#1a202c",
+        },
       },
     },
   },
