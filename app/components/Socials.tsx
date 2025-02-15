57be76687c067eae73f8a8ccb5e2b8fdf6aeb366
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-02-16 03:12:53 +1100

    fix: slider colors and social colors

--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -2,7 +2,7 @@ import FadeScroll from "./FadeScroll";
 
 export default function Socials() {
   return (
-    <section className="bg-gray-800 py-12 px-6 text-center" id="socials">
+    <section className="bg-gray-900 py-12 px-6 text-center" id="socials">
       <FadeScroll>
         <h2 className="text-3xl font-semibold mb-6">Let&apos;s Connect</h2>
         <div className="flex justify-center gap-8">
