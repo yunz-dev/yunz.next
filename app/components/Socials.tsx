fff1eb8095ddf96e1d86ba6c0c15d4e2241aa828
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-02-15 21:10:47 +1100

    fix: unescaped characters

--- app/components/Socials.tsx
+++ app/components/Socials.tsx
@@ -1,7 +1,7 @@
 export default function Socials() {
   return (
     <section className="bg-gray-800 py-12 px-6 text-center" id="socials">
-      <h2 className="text-3xl font-semibold mb-6">Let's Connect</h2>
+      <h2 className="text-3xl font-semibold mb-6">Let&apos;s Connect</h2>
       <div className="flex justify-center gap-8">
         <a
           href="https://www.linkedin.com/in/yunzz"
