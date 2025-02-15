c321eb120723b133a27273aad8cf7e176aebc782
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-02-15 22:29:05 +1100

    feat: animation on subtext

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { Typewriter } from "react-simple-typewriter";
 import Link from "next/link";
 
 export default function Hero() {
@@ -5,7 +7,19 @@ export default function Hero() {
     <section className="bg-gray-800 text-white h-screen flex flex-col justify-center items-center text-center px-6">
       <div className="container mx-auto">
         <h1 className="text-5xl font-bold mb-4">This is Yunz</h1>
-        <p className="text-lg mb-6">A passionate backend developer and tech enthusiast</p>
+        <div className="text-3xl mb-6">
+        <span>Passionate in </span>
+        <Typewriter
+          words={["backend engineering.", "devops and cloud", "machine learning", "creating"]}
+          loop={0} // Infinite loop
+          cursor
+          cursorStyle="|"
+          typeSpeed={80}
+          deleteSpeed={50}
+          delaySpeed={1000}
+        />
+
+        </div>
         <div className="flex justify-center gap-6">
           <Link
             href="#projects"
