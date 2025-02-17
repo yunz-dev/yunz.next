da7e5956da39dbc3ae470b00fc511e394462f0ba
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-02-17 21:34:28 +1100

    feat: HASEGI!!!

--- app/fun/page.tsx
+++ app/fun/page.tsx
@@ -1,16 +1,22 @@
 "use client";
+import { useState } from "react";
+import { delay } from "framer-motion";
 export default function Fun() {
+  const [title, setTitle] = useState("Fun Page");
 
-  const spinHandler = () => {
+  const spinHandler = async () => {
+    setTitle("HASEGI!!!")
     document.body.style.transition = 'all 1s';
     document.body.style.transform = 'rotate(360deg)';
-    location.reload();
+    delay( () => {
+      location.reload()
+    }, 2000);
   };
 
 
   return (
     <div className="min-h-screen bg-gray-900 text-gray-100 flex flex-col justify-center items-center">
-      <h1 className="text-5xl mb-6">Fun Page</h1>
+      <h1 className="text-5xl mb-6">{title}</h1>
       <p className="text-lg mb-6">Yeah this doesn&apos;t work <strong>yet</strong></p>
       <p className="text-lg mb-6">Current Count: 0</p>
       <button
