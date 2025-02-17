d64296532b6bfad1237afa16a9b70e63f87cd98f
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-02-17 21:15:02 +1100

    feat: spin button

--- app/fun/page.tsx
+++ app/fun/page.tsx
@@ -1,4 +1,12 @@
+"use client";
 export default function Fun() {
+
+  const spinHandler = () => {
+    document.body.style.transition = 'all 1s';
+    document.body.style.transform = 'rotate(360deg)';
+    location.reload();
+  };
+
 
   return (
     <div className="min-h-screen bg-gray-900 text-gray-100 flex flex-col justify-center items-center">
@@ -9,6 +17,13 @@ export default function Fun() {
         className="px-6 py-3 bg-gray-700 text-white rounded-lg hover:bg-gray-600 transition"
       >
         Increment Count
+      </button>
+      <br></br>
+      <button
+        onClick={spinHandler}
+        className="my-10 px-6 py-3 bg-gray-700 text-white rounded-lg hover:bg-gray-600 transition"
+      >
+        🌪️🌪️🌪️
       </button>
     </div>
   );
