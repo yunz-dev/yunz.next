f601487f4eaa07b20514b84e9725b02a1ad82e86
yunz-dev <112053367+yunz-dev@users.noreply.github.com>
2025-02-19 02:49:20 +1100

    add: name to about

--- app/components/About.tsx
+++ app/components/About.tsx
@@ -7,7 +7,7 @@ export default function About() {
         <FadeScroll>
           <h2 className="text-3xl font-semibold mb-4">About Me</h2>
           <p className="text-lg text-gray-400">
-            I’m a backend developer with a passion for building scalable, performant systems.
+            Hi I&apos;m Yunus, a backend developer with a passion for building scalable, performant systems.
             I specialize in working with modern technologies like Go, Java, and Rust, and I
             have a strong interest in cloud platforms like AWS and Azure. Also recently starting to integrate ML/AI in some of my projects.
           </p>
